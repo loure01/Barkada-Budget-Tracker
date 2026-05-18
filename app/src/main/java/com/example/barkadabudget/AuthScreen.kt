e20d9855bbb691b91e142587e2e83289d923fde5
rasce-chan <andrewdionisio.ras@hcdc.edu.ph>
2026-05-18 16:52:17 +0800

    ras: headline medium to large

--- app/src/main/java/com/example/barkadabudget/AuthScreen.kt
+++ app/src/main/java/com/example/barkadabudget/AuthScreen.kt
@@ -26,7 +26,7 @@ fun AuthScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(if (isRegister) "Create Group Account" else "Welcome to Barkada Budget", style = MaterialTheme.typography.headlineMedium)
+            Text(if (isRegister) "Create Group Account" else "Welcome to Barkada Budget", style = MaterialTheme.typography.headlineLarge)
             Spacer(modifier = Modifier.height(16.dp))
             OutlinedTextField(value = usernameValue, onValueChange = onUsernameChange, label = { Text(if (isRegister) "Choose Username" else "Username") }, modifier = Modifier.fillMaxWidth())
             Spacer(modifier = Modifier.height(8.dp))
